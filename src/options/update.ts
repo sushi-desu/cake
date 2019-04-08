269dfbf9194479374107ebacfad833bee3b8004f
hugemerge <hugemerge@gmail.com>
2019-04-08 17:23:33 +0900

    Update

--- src/options/update.ts
+++ src/options/update.ts
@@ -1,16 +1,12 @@
 import { IOptionState, IShopItem } from "../inventory";
 
 interface IOptionUpdate {
-  onInput: (e: Event) => (state: IOptionState) => IOptionState;
   add: (item: IShopItem) => (state: IOptionState) => IOptionState;
   delete: (id: string) => (state: IOptionState) => IOptionState;
   edit: (id: string, item: IShopItem) => (state: IOptionState) => IOptionState;
 }
 
 export const update: IOptionUpdate = {
-  onInput: e => state => {
-    return state;
-  },
 
   add: item => state => {
     return state;
