70bb2fa176c6b0ba6b958f542e8470a12fddaad5
hugemerge <hugemerge@gmail.com>
2019-04-11 12:03:47 +0900

    Fix type

--- src/ts/chromeApi.ts
+++ src/ts/chromeApi.ts
@@ -6,10 +6,10 @@ export const getLocalStorage = (): IShopItem[] => {
     {
       id: 'hogehoge',
       name: '松尾ミユキ ミルクガラス皿 ねこ',
-      price: 600,
-      weight: 235,
-      rakuten_stock: 3,
-      makeshop_stock: 3,
+      price: '600',
+      weight: '235',
+      rakuten_stock: '3',
+      makeshop_stock: '3',
       jancode: "4589533595659",
       descriptions: [{ title: "松尾ミユキ ミルクガラス皿 ねこ", body: "名古屋生まれのイラストレーター松尾ミユキさんデザインの真っ白なお皿が登場しました。シンプルでかわいいデザインなので、どんなシーンでもお使いいただけます。" }],
       details: [{ title: "商品名", body: "松尾ミユキ Milk Glass Dish Cat" }, { title: "サイズ", body: "直径 17.5cm・厚さ 17mm" }]
