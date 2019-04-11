0f1d2700ecad935b1e4e38515574f27fc5e3185c
hugemerge <hugemerge@gmail.com>
2019-04-11 15:53:47 +0900

    Add test data

--- src/ts/chromeApi.ts
+++ src/ts/chromeApi.ts
@@ -13,6 +13,17 @@ export const getLocalStorage = (): IShopItem[] => {
       jancode: "4589533595659",
       descriptions: [{ title: "松尾ミユキ ミルクガラス皿 ねこ", body: "名古屋生まれのイラストレーター松尾ミユキさんデザインの真っ白なお皿が登場しました。シンプルでかわいいデザインなので、どんなシーンでもお使いいただけます。" }],
       details: [{ title: "商品名", body: "松尾ミユキ Milk Glass Dish Cat" }, { title: "サイズ", body: "直径 17.5cm・厚さ 17mm" }]
+    },
+    {
+      id: 'test2',
+      name: '木のプレート',
+      price: '200',
+      weight: '120',
+      rakuten_stock: '2',
+      makeshop_stock: '4',
+      jancode: '010002555555',
+      descriptions: [{ title: 'おすすめ商品', body: '木でできたプレートです。'}, {title: '特徴的な色', body: '味わい深い色です。'}],
+      details: []
     }
   ]
 }
