84bd68d035446fc61591a6237005317d9ee59c15
hugemerge <hugemerge@gmail.com>
2019-04-23 21:10:09 +0900

    Add the function to connect to chrome storage

--- src/ts/chromeApi.ts
+++ src/ts/chromeApi.ts
@@ -1,18 +1,25 @@
 import { IShopItem } from "./item";
-import { uniqueId } from "./util"
 
-export const getItemlist = (): IShopItem[] => {
-
+export const getItemlist = (): Promise<IShopItem[]> => {
+  return new Promise<IShopItem[]>(resolve => {
+    chrome.storage.local.get('items', res => {
+      resolve(res['items']);
+    })
+  })
 }
 
-export const getLastSelectedId = (): string | null => {
-
+export const getLastSelectedId = (): Promise<string | null> => {
+  return new Promise<string | null>(resolve => {
+    chrome.storage.local.get('id', res => {
+      resolve(res['id']);
+    })
+  })
 }
 
 export const setItemlist = (array: IShopItem[]): void => {
-
+  chrome.storage.local.set({'items': array})
 }
 
 export const setLastSelectedId = (id: string | null): void => {
-
+  chrome.storage.local.set({'id': id})
 }
