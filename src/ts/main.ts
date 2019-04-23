5376625ef21a716ac7f0f9f0719b734bab97401e
hugemerge <hugemerge@gmail.com>
2019-04-23 15:57:41 +0900

    Add jancode validation

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -64,6 +64,22 @@ const validation = (): void => {
 }
 
 const is_valid = (form: HTMLFormElement): boolean => {
+  form.jancode.setCustomValidity('');
+  if (form.jancode.value.length === 13) {
+    const jan: string[] = form.jancode.value.split('');
+    const code = jan.slice(0, 12);
+    let od = code.filter((_, i) => i % 2 === 0).map(n => parseInt(n));
+    let ev = code.filter((_,i) => i % 2 === 1).map(n => parseInt(n));
+    if( od.some(n => isNaN(n)) || ev.some(n => isNaN(n)) ) {
+      form.jancode.setCustomValidity('数値を入力してください')
+    }
+    const total = od.reduce((acc, n) => acc + n) + ev.reduce((acc, n) => acc + n)*3
+
+    if ( (10 - (total % 10)).toString() !== form.jancode.value[12] ) {
+      form.jancode.setCustomValidity('JANコードが正しくありません')
+    }
+  }
+  document.querySelector('.help').innerHTML = form.jancode.validationMessage;
   return form.checkValidity();
 }
 
