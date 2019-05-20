c81e346b99948f169fa7b5a6bcf13d1194f05f69
hugemerge <hugemerge@gmail.com>
2019-05-20 12:42:54 +0900

    Add a script file for navbar

--- src/options/options.ts
+++ src/options/options.ts
@@ -1,12 +1,14 @@
 import { Model } from "../ts/model";
 import { setMainEventListener } from "../ts/main";
 import { setSidebarEventListener } from "../ts/sidebar";
+import { setNavbarEventListener } from "../ts/navbar";
 
 
 const model = new Model();
 model.ready.then(() => {
   setMainEventListener(model);
   setSidebarEventListener(model);
+  setNavbarEventListener();
 
   // initialize
   model.dispatchEvents();
